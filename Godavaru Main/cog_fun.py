a4c150a2bcc4e6477c271b431f852c65001fcaf9
Desiree <diallusions@gmail.com>
2017-07-12 18:00:35 -0400

    Fixed aliases (might start working on this again lol)

--- Godavaru Main/cog_fun.py
+++ Godavaru Main/cog_fun.py
@@ -55,63 +55,13 @@ class Fun():
         embed = discord.Embed(title='Press F to pay respects!',description='**' + ctx.message.author.display_name + '** has paid their respects successfully :eggplant:',color=ctx.message.author.color).set_footer(text='f')
         await self.bot.send_message(ctx.message.channel, content=None, embed=embed)
         
-    @commands.command(pass_context = True)
+    @commands.command(pass_context = True, aliases=["mb", "8ball"])
     async def magicball(self, ctx):
         random.seed(time.time())
         var = int(random.random() * 20)
 
-        if (ctx.message.content[12:] == ""):
-            await self.bot.say("I can't be a magician unless you ask me a question! <:thonking:294428719227994113>")
-        elif (var == 0):
-            await self.bot.say(":crystal_ball: It is certain.")
-        elif (var == 1):
-            await self.bot.say(":crystal_ball: It is decidedly so.")
-        elif (var == 2):
-            await self.bot.say(":crystal_ball: Without a doubt.")
-        elif (var == 3):
-            await self.bot.say(":crystal_ball: Yes, definitely.")
-        elif (var == 4):
-            await self.bot.say(":crystal_ball: You may rely on it.")
-        elif (var == 5):
-            await self.bot.say(":crystal_ball: As I see it, yes.")
-        elif (var == 6):
-            await self.bot.say(":crystal_ball: Most likely.")
-        elif (var == 7):
-            await self.bot.say(":crystal_ball: Outlook good.")
-        elif (var == 8):
-            await self.bot.say(":crystal_ball: Yes.")
-        elif (var == 9):
-            await self.bot.say(":crystal_ball: Signs point to yes.")
-        elif (var == 10):
-            await self.bot.say(":crystal_ball: Reply hazy, try again.")
-        elif (var == 11):
-            await self.bot.say(":crystal_ball: Ask again later.")
-        elif (var == 12):
-            await self.bot.say(":crystal_ball: Better not tell you now.")
-        elif (var == 13):
-            await self.bot.say(":crystal_ball: Cannot predict now.")
-        elif (var == 14):
-            await self.bot.say(":crystal_ball: Concentrate and ask again.")
-        elif (var == 15):
-            await self.bot.say(":crystal_ball: Don't count on it.")
-        elif (var == 16):
-            await self.bot.say(":crystal_ball: My reply is no.")
-        elif (var == 17):
-            await self.bot.say(":crystal_ball: My sources say no.")
-        elif (var == 18):
-            await self.bot.say(":crystal_ball: Outlook not so good.")
-        elif (var == 19):
-            await self.bot.say(":crystal_ball: Very doubtful")
-        elif (var == 20):
-            await self.bot.say(":crystal_ball: Congratulations, you found an easter egg. I hope you realise this doesn't answer your question...")
-
-    @commands.command(pass_context = True)
-    async def mb(self, ctx):
-        random.seed(time.time())
-        var = int(random.random() * 20)
-
-        if (ctx.message.content[5:] == ""):
-            await self.bot.say("I can't be a magician unless you ask me a question! <:thonking:294428719227994113>")
+        if (ctx.message.content == bot.command_prefix +"mb" or ctx.message.content == bot.command_prefix+"magicball" or ctx.message.content == bot.command_prefix+"8ball"):
+            await self.bot.say("I can't be a magician unless you ask me a question! <:thonk:316105720548556801>")
         elif (var == 0):
             await self.bot.say(":crystal_ball: It is certain.")
         elif (var == 1):
