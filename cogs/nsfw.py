30eab1c5efc0ceb158383180ef46e9b1853a617e
Desii <diallusions@gmail.com>
2018-04-28 11:02:37 -0400

    Would help to make things use my new features

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -3,6 +3,7 @@ import aiohttp
 import random
 import json
 from discord.ext import commands
+from .utils import checks
 
 
 class NSFW:
@@ -10,6 +11,7 @@ class NSFW:
         self.bot = bot
 
     @commands.command(usage="<members>")
+    @checks.is_nsfw()
     @commands.bot_has_permissions(embed_links=True)
     async def fuck(self, ctx):
         """Feeling lewd? Why don't you go and fuck a person :eyes:
@@ -41,8 +43,6 @@ class NSFW:
             "https://angrygif.com/wp-content/uploads/2017/02/fd5d61e746e1dc4a41b872f4e388753f.gif"]
         if len(ctx.message.mentions) == 0:
             return await ctx.send(":x: You can't fuck the air... well, you can try.")
-        if not ctx.channel.is_nsfw():
-            return await ctx.send(":x: Y-you lewdie! Go get a room!")
         msg = f"**{ctx.author.display_name}** is fucking **{(', '.join([m.display_name for m in ctx.message.mentions])).replace(', '+ctx.message.mentions[len(ctx.message.mentions)-1].display_name, ' and '+ctx.message.mentions[len(ctx.message.mentions)-1].display_name)}**!"
         if ctx.author in ctx.message.mentions:
             msg = 'Oh, okay. Sure. I\'ll fuck you.'
@@ -54,29 +54,27 @@ class NSFW:
         await ctx.send(embed=em)
 
     @commands.command(aliases=["r34"])
+    @checks.is_nsfw()
     async def rule34(self, ctx, tag: str):
         """Search for an image on rule34!
         Note: To use this command, the channel must be NSFW."""
-        if ctx.channel.is_nsfw():
-            try:
-                url = 'https://rule34.xxx/index.php?page=dapi&s=post&q=index&json=1&tags=' + tag
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(url) as resp:
-                        js = json.loads(await resp.text())
-                non_loli = list(filter(lambda x: 'loli' not in x['tags'] and 'shota' not in x['tags'], js))
-                if len(non_loli) == 0:
-                    return await ctx.send(":warning: All results included loli/shota content; this search is invalid.")
-                response = non_loli[random.randint(0, len(non_loli) - 1)]
-                img = f"https://img.rule34.xxx/images/{response['directory']}/{response['image']}"
-                tags = response['tags'].split(' ')
-                em = discord.Embed(description=f'`{", ".join(tags)}`', colour=0xff0000)
-                em.set_image(url=img)
-                em.set_author(name='Found Image! Click me if it doesn\'t load!', url=img)
-                await ctx.send(embed=em)
-            except json.JSONDecodeError:
-                await ctx.send(":x: No image found. Sorry :/")
-        else:
-            await ctx.send(":x: This is not an NSFW channel.")
+        try:
+            url = 'https://rule34.xxx/index.php?page=dapi&s=post&q=index&json=1&tags=' + tag
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as resp:
+                    js = json.loads(await resp.text())
+            non_loli = list(filter(lambda x: 'loli' not in x['tags'] and 'shota' not in x['tags'], js))
+            if len(non_loli) == 0:
+                return await ctx.send(":warning: All results included loli/shota content; this search is invalid.")
+            response = non_loli[random.randint(0, len(non_loli) - 1)]
+            img = f"https://img.rule34.xxx/images/{response['directory']}/{response['image']}"
+            tags = response['tags'].split(' ')
+            em = discord.Embed(description=f'`{", ".join(tags)}`', colour=0xff0000)
+            em.set_image(url=img)
+            em.set_author(name='Found Image! Click me if it doesn\'t load!', url=img)
+            await ctx.send(embed=em)
+        except json.JSONDecodeError:
+            await ctx.send(":x: No image found. Sorry :/")
 
     @commands.command()
     async def yandere(self, ctx, tag: str, rating: str = None):
