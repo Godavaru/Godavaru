c8c54372e7597cd002dc71cf48b7ee673e9145c3
Desii <diallusions@gmail.com>
2018-05-19 13:15:02 -0400

    fixes

--- cogs/events/logs.py
+++ cogs/events/logs.py
@@ -7,13 +7,12 @@ class Logs:
         self.bot = bot
 
     async def on_message_delete(self, message):
-        await self.bot.get_channel(315252624645423105).send('fuck')
         channel = get_log_channel(self.bot, message.guild)
         if channel and channel.permissions_for(message.guild.me).send_messages:
             content = '\n-'.join(escape_markdown(message.clean_content, True).split('\n'))
-            await channel.send(resolve_emoji('INFO', message)
+            await channel.send(resolve_emoji('ERROR', message)
                                + f' Message by **{message.author}** was deleted in {message.channel.mention}\n'
-                               + f'```diff\n{content}\n```')
+                               + f'```diff\n-{content}\n```')
 
 def setup(bot):
     bot.add_cog(Logs(bot))
